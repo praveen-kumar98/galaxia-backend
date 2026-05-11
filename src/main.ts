3db53ff6aee2fee572bd4c67549f5c479f050385
Praveen Kumar Koram <pra_win98@protonmail.com>
2026-05-11 11:24:59 +0530

    fit: remove duplicate app.listen() call in bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,8 @@ import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(CoreModule);
-  await app.listen(process.env.PORT ?? 3000);
-  
-  app.setGlobalPrefix("api/v1")
+
+  app.setGlobalPrefix("api/v1");
 
   app.enableVersioning({
     type: VersioningType.URI,
